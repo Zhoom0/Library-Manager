f42395cff0af41761249382b7df823d1cdd79a5e
Zhoom0 <neumatex0@gmail.com>
2026-06-15 17:56:06 -0400

    Se avanzó en el metodo para prestar un libro

--- src/services/biblioteca.py
+++ src/services/biblioteca.py
@@ -22,4 +22,17 @@ class Biblioteca:
         else:
             print("No se pudo agregar el socio, por favor ingrese un socio valido.")
 
-    
+    def prestar_libro(self, libro_para_prestar: Libro, socio_para_prestar: Socio) -> None:
+        if libro_para_prestar is None:
+            print("Por favor seleccione un libro valido.")
+            return None
+        if not libro_para_prestar.disponible:
+            print("ERROR: Este libro ya se encuentra prestado.")
+            return None
+        
+        if socio_para_prestar is None:
+            print("Por favor seleccione un socio valido.")
+            return None
+        if libro_para_prestar in socio_para_prestar.libros_en_posesion:
+            print("ERROR: Este socio ya posee ese libro.")
+            return None
